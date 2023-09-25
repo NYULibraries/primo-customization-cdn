ce8c463f3b771164a6d386274a774c2fc755546d
David <da70@nyu.edu>
2023-09-25 11:41:15 -0400

    primo-customization/01NYU_INST-NYU_DEV/js/custom.js: format according to `primo-customization` repo eslint rules

--- primo-customization/01NYU_INST-NYU_DEV/js/custom.js
+++ primo-customization/01NYU_INST-NYU_DEV/js/custom.js
@@ -1,4 +1,4 @@
-function findingAidsLinkClickHandler(event) {
+function findingAidsLinkClickHandler( event ) {
     event.stopPropagation();
 }
 
@@ -18,19 +18,21 @@ function getCdnUrl() {
 
 // thanks to https://stackoverflow.com/a/53601942
 function documentReady( func ) {
-  // if early to the DOM
-  document.addEventListener("DOMContentLoaded", func);
-  // if late to the DOM
-  if (document.readyState === "interactive" || document.readyState === "complete" ) {
-    func();
-  }
+    // if early to the DOM
+    document.addEventListener( 'DOMContentLoaded', func );
+    // if late to the DOM
+    if ( document.readyState === 'interactive' || document.readyState === 'complete' ) {
+        func();
+    }
 }
 
-documentReady(function() {
-  // load homepage from CDN
-  fetch(`${ cdnUrl }/html/additional/homepage_en.html`)
-    .then(function(response){ return response.text(); })
-    .then(function(text){ 
-      document.querySelector("prm-search md-content").innerHTML = text;
-    });
-});
+documentReady( function () {
+    // load homepage from CDN
+    fetch( `${ cdnUrl }/html/additional/homepage_en.html` )
+        .then( function ( response ) {
+            return response.text();
+        } )
+        .then( function ( text ) {
+            document.querySelector( 'prm-search md-content' ).innerHTML = text;
+        } );
+} );
