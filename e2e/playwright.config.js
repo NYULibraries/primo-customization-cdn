a698a2161a19d489fc166958ff391b30194b8b56
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-11-02 15:42:57 -0400

    PLAYWRIGHT: Revert timeout to 90 sec
    Signed-off-by: Vlad Burlutskiy <vlad.burlutskiy@icloud.com>

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -29,7 +29,7 @@ const config = {
 
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 1000,
+  timeout: 90 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
