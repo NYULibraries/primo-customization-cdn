30f15fadb47625179fc9b48f2c8f0f01f640f71a
David <da70@nyu.edu>
2023-11-29 00:02:02 -0500

    01NYU_INST-TESTWS01/js/custom.js: update from 01NYU_INST-NYU_DEV

--- primo-customization/01NYU_INST-TESTWS01/js/custom.js
+++ primo-customization/01NYU_INST-TESTWS01/js/custom.js
@@ -47,8 +47,29 @@ function configureAndInjectLibKey() {
     document.head.appendChild( browzine.script );
 }
 
+function injectStatusEmbed() {
+    // Always use prod URL for all views:
+    // https://nyu-lib.monday.com/boards/765008773/pulses/5525193850/posts/2571053345
+    const STATUS_EMBED_PROD_URL
+        = 'https://cdn.library.nyu.edu/statuspage-embed/index.min.js';
+    const scriptTag = document.createElement( 'script' );
+    scriptTag.setAttribute( 'src', STATUS_EMBED_PROD_URL );
+    document.body.appendChild( scriptTag )
+}
+
 configureAndInjectLibKey();
+injectStatusEmbed();
 
 function findingAidsLinkClickHandler( event ) {
     event.stopPropagation();
 }
+
+// chatwidget-embed
+( function () {
+    var s = document.createElement( 'script' );
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://cdn.library.nyu.edu/chatwidget-embed/index.min.js';
+    var x = document.getElementsByTagName( 'script' )[0];
+    x.parentNode.insertBefore( s, x );
+} )();
