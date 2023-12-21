9ac5e3abd68b3adb965016fbf58a399c092ee063
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-12-21 13:42:07 -0500

    testutils: Add helper function to modify CSP header
    Signed-off-by: Vlad Burlutskiy <vlad.burlutskiy@icloud.com>

--- e2e/testutils/index.js
+++ e2e/testutils/index.js
@@ -1,5 +1,3 @@
-const view = process.env.VIEW;
-
 function setPathAndQueryVid( pathAndQuery, vid ) {
     return pathAndQuery.replace( 'vid=[VID]', `vid=${ vid }` );
 }
@@ -9,7 +7,36 @@ function updateGoldenFiles() {
         process.env.UPDATE_GOLDEN_FILES.toLowerCase() !== 'false';
 }
 
-export {
+// Based on https://playwright.dev/docs/next/network#modify-responses
+async function modifyCSPHeader(page) {
+    await page.route('**/*', async route => {
+        const response = await route.fetch();
+        const originalHeaders = response.headers();
+
+        // Prepare the modified CSP header, if necessary
+        let csp = originalHeaders['content-security-policy'];
+        if (csp && csp.includes('upgrade-insecure-requests')) {
+
+            let directives = csp.split(';').map(dir => dir.trim());
+
+            directives = directives.filter(dir => !dir.toLowerCase().includes('upgrade-insecure-requests'));
+
+            csp = directives.join('; ').trim();
+        }
+
+        route.fulfill({
+            response,
+            headers: {
+                ...originalHeaders,
+                'content-security-policy': csp ? csp : originalHeaders['content-security-policy']
+            }
+        });
+    });
+}
+
+module.exports = {
+    modifyCSPHeader,
     setPathAndQueryVid,
-    updateGoldenFiles,
+    updateGoldenFiles
 };
+
