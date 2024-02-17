93e7979af0146bc67c58a19c431e35650f77e956
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2024-02-16 21:12:10 -0500

    chatWidget E2E: Add HTML and screenshot comparison
    Signed-off-by: Vlad Burlutskiy <vlad.burlutskiy@icloud.com>

--- e2e/tests/chat-widget.spec.js
+++ e2e/tests/chat-widget.spec.js
@@ -1,6 +1,13 @@
+import * as fs from 'node:fs';
+
+import { modifyCSPHeader, removeSourceMappingUrlComments, setPathAndQueryVid, updateGoldenFiles } from '../testutils';
+
+import { execSync } from 'node:child_process';
+
 const { test, expect } = require( '@playwright/test' );
 
-import { modifyCSPHeader, setPathAndQueryVid } from '../testutils';
+
+const beautifyHtml = require( 'js-beautify' ).html;
 
 const view = process.env.VIEW;
 
@@ -58,6 +65,67 @@ if ( viewsForTest.includes( view ) ) {
                 // staffers responsible for responding to chat requests.
                 expect( true ).toBe( true );
             } );
+
+            test( 'chat widget HTML matches expected', async ({ page }) => {
+                const actualHTMLFile = `tests/actual/${view}/chat-widget-${testCase.name}.html`;
+                try {
+                   fs.unlinkSync(actualHTMLFile);
+                } catch (error) {  }
+                const diffHTMLFile = `tests/diffs/${view}/chat-widget-${testCase.name}.txt`;
+                try {
+                    fs.unlinkSync(diffHTMLFile);
+                } catch (error) {  }
+
+                await page.locator( CHAT_WIDGET_SELECTOR ).waitFor();
+
+                const actualHTML = beautifyHtml(removeSourceMappingUrlComments(await page.locator( CHAT_WIDGET_SELECTOR ).innerHTML()));
+
+                const goldenFile = `tests/golden/${view}/chat-widget-${testCase.name}.html`;
+                if (updateGoldenFiles()) {
+                    fs.writeFileSync(goldenFile, actualHTML);
+
+                    console.log(`Updated golden file ${goldenFile}`);
+
+                    return;
+                }
+                const golden = beautifyHtml(fs.readFileSync(goldenFile, 'utf8'));
+
+                fs.writeFileSync(actualHTMLFile, actualHTML);
+
+                const ok = actualHTML === golden;
+
+                let message = `Actual HTML for "${testCase.name}" does not match expected HTML`;
+                if (!ok) {
+                    const command = `diff ${goldenFile} ${actualHTMLFile} | tee ${diffHTMLFile}`;
+                    let diffOutput;
+                    try {
+                        diffOutput = new TextDecoder().decode(execSync(command));
+                        message += `
+
+======= BEGIN DIFF OUTPUT ========
+${diffOutput}
+======== END DIFF OUTPUT =========
+
+[Recorded in diff file: ${diffFile}]`;
+                    } catch (e) {
+                        // `diff` command failed to create the diff file.
+                        message += `  Diff command \`${command}\` failed:
+
+            ${e.stderr.toString()}`;
+                    }
+                    }
+
+                expect(ok, message).toBe(true);
+            });
+
+            test( 'chat widget screenshot matches expected', async ({ page }) => {
+                // test.skip(process.platform === 'darwin', 'This test is not implemented for Mac');
+
+                await page.locator( CHAT_WIDGET_SELECTOR ).waitFor();
+
+                await expect( page.locator( CHAT_WIDGET_SELECTOR ) ).toHaveScreenshot( `chat-widget-${testCase.name}.png` );
+            });
+
         } ) // End `test.describe(...)`
     } // End `testCases` for-loop
 } // End `if ( viewsForTest.includes( view ) )`
