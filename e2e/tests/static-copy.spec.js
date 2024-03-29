9072e13946580b1fc85409a7b7bcdc53a213bb8d
David <da70@nyu.edu>
2024-03-29 17:28:28 -0400

    e2e/tests/static-copy.spec.js: re-enable no-search-results test

--- e2e/tests/static-copy.spec.js
+++ e2e/tests/static-copy.spec.js
@@ -18,13 +18,13 @@ const testCases = [
         elementToTest   : 'prm-static',
         waitForSelector : 'md-card[ data-cy="home-need-help" ]',
     },
-    // {
-    //     key: 'no-search-results',
-    //     name: 'No-search-results page',
-    //     pathAndQuery: '/discovery/search?vid=[VID]&query=any,contains,gasldfjlak%3D%3D%3Dasgjlk%26%26%26%26!!!!&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
-    //     elementToTest: 'prm-no-search-result',
-    //     waitForSelector: 'prm-no-search-result-after',
-    // },
+    {
+        key: 'no-search-results',
+        name: 'No-search-results page',
+        pathAndQuery: '/discovery/search?vid=[VID]&query=any,contains,gasldfjlak%3D%3D%3Dasgjlk%26%26%26%26!!!!&tab=Unified_Slot&search_scope=DN_and_CI&offset=0',
+        elementToTest: 'prm-no-search-result',
+        waitForSelector: 'prm-no-search-result-after',
+    },
 ];
 
 for (let i = 0; i < testCases.length; i++) {
