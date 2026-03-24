517d461a6c3b326505b3818ab4ae3ce98559a0a2
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2026-03-24 18:09:48 -0400

    E2E: update health check endpoint and add readiness check in entrypoint script

--- e2e/docker-entrypoint.sh
+++ e2e/docker-entrypoint.sh
@@ -5,4 +5,17 @@ cd /unit_tests
 yarn test
 
 cd /e2e
+echo "Waiting for app readiness..."
+APP_READY_URL="${PLAYWRIGHT_BASE_URL:-https://e2e.nyu.primo.exlibrisgroup.com}/discovery/search"
+APP_READY_TIMEOUT_SECONDS=120
+APP_READY_DEADLINE=$(( $(date +%s) + APP_READY_TIMEOUT_SECONDS ))
+
+until curl -ksf "$APP_READY_URL" >/dev/null; do
+  if [ "$(date +%s)" -ge "$APP_READY_DEADLINE" ]; then
+    echo "App did not become ready at $APP_READY_URL" >&2
+    exit 1
+  fi
+  sleep 2
+done
+
 exec "$@"
