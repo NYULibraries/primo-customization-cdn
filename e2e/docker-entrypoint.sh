3c7037e4f2a7da0cd2178a44da57f86fd128d17d
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2026-03-24 17:14:21 -0400

    E2E: update service dependencies to use health checks

--- e2e/docker-entrypoint.sh
+++ e2e/docker-entrypoint.sh
@@ -1,28 +1,8 @@
 #!/bin/sh -e
 
-# Navigate to unit_tests directory and run unit tests
 echo "Running unit tests..."
 cd /unit_tests
 yarn test
-cd .. && cd e2e
 
-
-start_time=$(date +%s)
-timeout=30 #seconds
-checkUrl="${E2E_READY_URL:-http://primo-explore-devenv:8003/discovery/search}"
-curlArgs="-fsS"
-case "$checkUrl" in
-  https://*) curlArgs="$curlArgs -k" ;;
-esac
-
-while ! curl $curlArgs "$checkUrl" -o /dev/null; do
-  sleep 3
-  now=$(date +%s)
-  if [ $(( now - start_time )) -gt $timeout ]; then
-    echo "Unable to connect to $checkUrl after $timeout seconds; aborting!"
-    exit 1
-  fi
-done
-
+cd /e2e
 exec "$@"
-
